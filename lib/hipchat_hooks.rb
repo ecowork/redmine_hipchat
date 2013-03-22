2d06820901ef58ccdb2fdb6ee2aee5d9388cf45a
xdite <xuite.joke@gmai.com>
2013-03-22 18:00:33 +0800

    refactor controller_issues_new_after_save
    controller_issues_edit_before_save
    
    issue_assigned_changed?
    
    issue_status_changed?
    
    add tracker
    
    url
    
    subject
    
    拿掉 after save
    
    wiki

--- lib/hipchat_hooks.rb
+++ lib/hipchat_hooks.rb
@@ -11,7 +11,12 @@ class NotificationHook < Redmine::Hook::Listener
     tracker = CGI::escapeHTML(issue.tracker.name.downcase)
     subject = CGI::escapeHTML(issue.subject)
     url     = get_url(issue)
-    text    = "#{author} reported #{project.name} #{tracker} <a href=\"#{url}\">##{issue.id}</a>: #{subject}"
+
+    assigned_message = issue.assigned_to.nil? ? "NULL" : "#{issue.assigned_to.name}"
+
+    text =   "#{author} 建立 #{tracker} <a href='#{url}'> ##{issue.id} </a> : #{subject}"
+    text +=  "狀態:「#{issue.status.name}」. 分派給:「#{assigned_message}」. 意見:「#{truncate(issue.description)}」"
+
 
     data          = {}
     data[:text]   = text
@@ -22,18 +27,22 @@ class NotificationHook < Redmine::Hook::Listener
     send_message(data)
   end
 
-  def controller_issues_edit_after_save(context = {})
+
+
+  def controller_issues_edit_before_save(context = { })
     issue   = context[:issue]
     project = issue.project
-    return true if !hipchat_configured?(project)
-
-    author  = CGI::escapeHTML(User.current.name)
+    journal = context[:journal]
+    editor = journal.user
     tracker = CGI::escapeHTML(issue.tracker.name.downcase)
-    subject = CGI::escapeHTML(issue.subject)
-    comment = CGI::escapeHTML(context[:journal].notes)
+    assigned_message = issue_assigned_changed?(issue)
+    status_message = issue_status_changed?(issue)
     url     = get_url(issue)
-    text    = "#{author} updated #{project.name} #{tracker} <a href=\"#{url}\">##{issue.id}</a>: #{subject}"
-    text   += ": <i>#{truncate(comment)}</i>" unless comment.blank?
+    subject = CGI::escapeHTML(issue.subject)
+
+    text = ""
+    text += "#{editor.name} 編輯 #{tracker} <a href='#{url}'> ##{issue.id} </a> : #{subject}"
+    text += "狀態:「#{status_message}」. 分派給:「#{assigned_message}」. 意見:「#{truncate(journal.notes)}」"
 
     data          = {}
     data[:text]   = text
@@ -43,6 +52,8 @@ class NotificationHook < Redmine::Hook::Listener
 
     send_message(data)
   end
+
+
 
   def controller_wiki_edit_after_save(context = {})
     page    = context[:page]
@@ -53,7 +64,10 @@ class NotificationHook < Redmine::Hook::Listener
     wiki         = CGI::escapeHTML(page.pretty_title)
     project_name = CGI::escapeHTML(project.name)
     url          = get_url(page)
-    text         = "#{author} edited #{project_name} wiki page <a href=\"#{url}\">#{wiki}</a>"
+    text         = "#{author} edited #{project_name} wiki page <a href='#{url}'>#{wiki}</a>"
+
+    text = ""
+    text += "#{author} edited the  <a href='#{url}'>#{wiki}</a> on #{project.name}."
 
     data          = {}
     data[:text]   = text
@@ -70,9 +84,9 @@ class NotificationHook < Redmine::Hook::Listener
     if !project.hipchat_auth_token.empty? && !project.hipchat_room_name.empty?
       return true
     elsif Setting.plugin_redmine_hipchat[:projects] &&
-          Setting.plugin_redmine_hipchat[:projects].include?(project.id.to_s) &&
-          Setting.plugin_redmine_hipchat[:auth_token] &&
-          Setting.plugin_redmine_hipchat[:room_id]
+        Setting.plugin_redmine_hipchat[:projects].include?(project.id.to_s) &&
+        Setting.plugin_redmine_hipchat[:auth_token] &&
+        Setting.plugin_redmine_hipchat[:room_id]
       return true
     else
       Rails.logger.info "Not sending HipChat message - missing config"
@@ -97,8 +111,8 @@ class NotificationHook < Redmine::Hook::Listener
 
   def get_url(object)
     case object
-      when Issue    then "#{Setting[:protocol]}://#{Setting[:host_name]}/issues/#{object.id}"
-      when WikiPage then "#{Setting[:protocol]}://#{Setting[:host_name]}/projects/#{object.wiki.project.identifier}/wiki/#{object.title}"
+    when Issue    then "#{Setting[:protocol]}://#{Setting[:host_name]}/issues/#{object.id}"
+    when WikiPage then "#{Setting[:protocol]}://#{Setting[:host_name]}/projects/#{object.wiki.project.identifier}/wiki/#{object.title}"
     else
       Rails.logger.info "Asked redmine_hipchat for the url of an unsupported object #{object.inspect}"
     end
@@ -108,11 +122,11 @@ class NotificationHook < Redmine::Hook::Listener
     Rails.logger.info "Sending message to HipChat: #{data[:text]}"
     req = Net::HTTP::Post.new("/v1/rooms/message")
     req.set_form_data({
-      :auth_token => data[:token],
-      :room_id => data[:room],
-      :notify => data[:notify] ? 1 : 0,
-      :from => 'Redmine',
-      :message => data[:text]
+                        :auth_token => data[:token],
+                        :room_id => data[:room],
+                        :notify => data[:notify] ? 1 : 0,
+                        :from => 'Redmine',
+                        :message => data[:text]
     })
     req["Content-Type"] = 'application/x-www-form-urlencoded'
 
@@ -132,5 +146,26 @@ class NotificationHook < Redmine::Hook::Listener
     return unless text
     words = text.split()
     words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  end
+
+  def issue_status_changed?(issue)
+    if issue.status_id_changed?
+      old_status = IssueStatus.find(issue.status_id_was)
+      "從 #{old_status.name} 變更為 #{issue.status.name}"
+    else
+      "#{issue.status.name}"
+    end
+  end
+
+
+  def issue_assigned_changed?(issue)
+    if issue.assigned_to_id_changed?
+      old_assigned_to = User.find(issue.assigned_to_id_was) rescue nil
+      old_assigned = old_assigned_to.nil? ? "無" : "#{old_assigned_to.lastname} #{old_assigned_to.firstname}"
+      new_assigned = issue.assigned_to.nil? ? "無" : "#{issue.assigned_to.lastname} #{issue.assigned_to.firstname}"
+      "從 #{old_assigned} 變更為 #{new_assigned}"
+    else
+      issue.assigned_to.nil? ? "無" : "#{issue.assigned_to.lastname} #{issue.assigned_to.firstname}"
+    end
   end
 end
